56b94668728f2068f6ecd4c57fa39b996d74f03f
Aaron Bieber <qbit@deftly.net>
2012-02-18 06:38:01 -0700

    removed async, added debug, added orientation / coderwall_element

--- test/www_test.js
+++ test/www_test.js
@@ -5,6 +5,8 @@ var app = express.createServer();
 
 var a = new coderwall( {
 	user: 'qbit',
+	debug: true,
+	orientation: 'vertical',
 	images: __dirname + '/images',
 	url: '/coderwall/'
 });
@@ -14,7 +16,8 @@ a.get();
 a.connect( app );
 
 app.get( '/', function( req,res ) {
-	res.send( '<html><head><link href="/coderwall/coderwall.css" media="all" rel="stylesheet" type="text/css"><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><script src="/coderwall/coderwall.js" type="text/javascript"></script></head><body><section class="coderwall" data-coderwall-username="qbit" data-coderwall-orientation="vertical"></section></body></html>' );
+	res.send( '<html><head><link href="/coderwall/coderwall.css" media="all" rel="stylesheet" type="text/css"><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><script src="/coderwall/coderwall.js" type="text/javascript"></script></head><body>' + a.coderwall_element + '</body></html>' );
 });
 
+console.log( "listening on http://localhost:8080" );
 app.listen( 8080 );
